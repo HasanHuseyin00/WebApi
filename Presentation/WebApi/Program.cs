e5816f492ebd5cd4aa6913316824f7f51c4282c3
HasanHuseyin00 <HasanHuseyin.AK@hotmail.com>
2022-07-08 04:44:40 +0300

    AddDbContextPool

--- Presentation/WebApi/Program.cs
+++ Presentation/WebApi/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-builder.Services.AddDbContext<DatabaseContext>(options =>
+builder.Services.AddDbContextPool<DatabaseContext>(options =>
        options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
